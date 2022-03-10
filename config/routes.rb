0af128ead5f8d0687f3f1b93434a064c780acc4e
ptnguyen728 <ptnguyen728@gmail.com>
2022-03-10 08:57:40 +0000

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  #root 'controller_name#action_name'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   # root 'welcome#index'
+   root 'application#hello'
+end
